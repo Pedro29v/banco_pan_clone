fb8ae93d98e07937e447c8656af545eab09b82f0
unknown <pedrocastillo24.pc@gmail.com>
2024-12-06 13:48:40 -0300

    create card option interface

--- app/src/main/java/com/pedro/banco_pan_clone/CardActivity.kt
+++ app/src/main/java/com/pedro/banco_pan_clone/CardActivity.kt
@@ -1,28 +1,20 @@
 package com.pedro.banco_pan_clone
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-/*        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_card)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }*/
-
-        val button = findViewById<LinearLayout>(R.id.button_card)
-        button.setOnClickListener {
-            val intent = Intent(this, CardActivity::class.java)
-            startActivity(intent)
         }
     }
 }
