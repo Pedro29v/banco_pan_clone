5604925df06487d68ee77788389620da60ebb3ab
unknown <pedrocastillo24.pc@gmail.com>
2024-12-08 13:49:56 -0300

    solves problem in lanscape mode

--- app/src/main/java/com/pedro/banco_pan_clone/MainActivity.kt
+++ app/src/main/java/com/pedro/banco_pan_clone/MainActivity.kt
@@ -13,11 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-/*        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
 
         val button = findViewById<LinearLayout>(R.id.button_card)
         button.setOnClickListener {
